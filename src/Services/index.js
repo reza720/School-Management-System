36ad47682d7ec9d2ac398f1b4884f822a611a266
Reza Panahi <rezabumanpanahi@gmail.com>
2026-01-05 12:54:59 +0430

    feat:index of services added

--- src/Services/index.js
+++ src/Services/index.js
@@ -1,1 +1,15 @@
-const TeacherRepo=require("./TeacherService");
+const TeacherService=require("./TeacherService");
+const StudentService=require("./StudentService");
+const CourseService=require("./CourseService");
+const ExamService=require("./ExamService");
+const AttendanceService=require("./AttendanceService");
+const StudentCoursesService=require("./StudentCoursesService");
+
+module.exports={
+    TeacherService,
+    StudentService,
+    CourseService,
+    ExamService,
+    AttendanceService,
+    StudentCoursesService
+};
